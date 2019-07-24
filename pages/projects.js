8dadbb461a418082e8457334091cfce0d727edaa
Jack Fletcher <jdfletch97@gmail.com>
2019-07-24 17:31:32 -0500

    Add project cards to projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import {ContentContainer, Meta, Page} from '../components';
+import {Card, ContentContainer, Meta, Page} from '../components';
 import {getBaseUrl} from '../lib';
+
+const projects = {
+  splash: {
+    color: '#FEC006',
+    title: 'Splash',
+    thumbnail: '',
+    category: 'Data',
+    link: 'https://splash.jackfletch.com',
+    excerpt: 'NBA Shooting Visualization',
+    date: new Date(2017, 4, 29),
+  },
+  thesis: {
+    color: '#2196F3',
+    title: 'Undergraduate Thesis',
+    thumbnail: '',
+    category: 'Research',
+    link: 'https://jackfletch.com/static/thesis.pdf',
+    excerpt: 'Analysis of an Embedded Device using Machine Learning',
+    date: new Date(2019, 5, 26),
+  },
+  skipLists: {
+    color: '#FE5621',
+    title: 'Skip Lists',
+    thumbnail: '',
+    category: 'Class Project',
+    link: 'https://github.com/jackfletch/CSE2383-SkipList',
+    excerpt:
+      'Honors Project for CSE 2383 Data Stuctures and Analysis of Algorithms',
+    date: new Date(2016, 11, 12),
+  },
+  dotfiles: {
+    color: '#673AB7',
+    title: 'Dotfiles',
+    thumbnail: '',
+    category: 'Config',
+    link: 'https://github.com/jackfletch/dotfiles',
+    excerpt: 'My dotfiles',
+    date: new Date(),
+  },
+  yahtzee: {
+    color: '#FE5621',
+    title: 'Yahtzee GUI',
+    thumbnail: '',
+    category: 'Class Project',
+    link: '#',
+    excerpt: 'Yahtzee game implementation with a GUI',
+    date: new Date(),
+  },
+};
+
+const innerContentContainerStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  width: '90%',
+  maxWidth: '960px',
+  margin: '0 auto',
+};
+
+const outerContentContainerStyle = {
+  marginTop: '60px',
+};
 
 const H2 = styled.h2`
   margin-top: 60px;
@@ -29,8 +91,13 @@ class ProjectsPage extends React.Component {
       <>
         <Meta baseUrl={baseUrl} staticPage={{title}} />
         <Page baseUrl={baseUrl}>
-          <ContentContainer>
-            <H2>This is where I talk about my software projects.</H2>
+          <ContentContainer
+            innerStyle={innerContentContainerStyle}
+            outerStyle={outerContentContainerStyle}
+          >
+            {Object.keys(projects).map(project => (
+              <Card key={project} value={projects[project]} />
+            ))}
           </ContentContainer>
         </Page>
       </>
