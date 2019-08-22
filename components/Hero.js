b6593705a6b864a569e5b89997806592b12615d7
Jack Fletcher <jdfletch97@gmail.com>
2019-08-22 14:53:17 -0500

    Remove portrait image from index page

--- components/Hero.js
+++ components/Hero.js
@@ -5,38 +5,18 @@ import styled from 'styled-components';
 const HeroDiv = styled.div`
   align-items: center;
   display: flex;
-  flex-direction: column;
   height: 100vh;
-  justify-content: space-evenly;
-  @media screen and (min-width: 992px) {
-    flex-direction: row;
-    justify-content: unset;
-  }
 `;
 
 const MessageDiv = styled.div`
-  flex: 0;
-  width: 100%;
   padding: 2.5em 15px;
-  @media screen and (min-width: 992px) {
-    flex: 1;
-  }
+  width: 100%;
 `;
 
 const H1 = styled.h1`
   margin: 0;
   @media screen and (max-width: 991px) {
     font-size: 3rem;
-  }
-`;
-
-const ImgDiv = styled.div`
-  display: none;
-  @media screen and (min-width: 992px) {
-    display: block;
-  }
-  @media screen and (min-height: 750px) {
-    display: block;
   }
 `;
 
@@ -62,17 +42,6 @@ const Hero = () => (
           .
         </H1>
       </MessageDiv>
-      <ImgDiv>
-        <img
-          src="/static/img/hero_corner.png"
-          alt="Jack Fletcher"
-          style={{
-            objectFit: 'cover',
-            width: '300px',
-            height: '400px',
-          }}
-        />
-      </ImgDiv>
     </HeroDiv>
   </>
 );
