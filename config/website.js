cdc8dff0182030b9e23e78a2d54a6bf3fa1089ac
Jack Fletcher <jdfletch97@gmail.com>
2019-08-26 13:18:03 -0500

    Add website config

---
+++ config/website.js
@@ -0,0 +1,31 @@
+const websiteConfig = {
+  author: {
+    name: 'Jack Fletcher',
+    contacts: {
+      email: 'jdfletch97@gmail.com',
+      github: 'jackfletch',
+      instagram: 'fletch_jack',
+      linkedin: 'jdfletch',
+      twitter: 'no_fletch_zone',
+    },
+  },
+  title: 'Fletcher Labs',
+  description:
+    "CPE '19 @ Mississippi State. I blog about web development, machine learning, visualization, and more.",
+  menu: [
+    {
+      label: 'Welcome',
+      path: '/',
+    },
+    {
+      label: 'Blog',
+      path: '/blog',
+    },
+    {
+      label: 'Projects',
+      path: '/projects',
+    },
+  ],
+};
+
+export default websiteConfig;
