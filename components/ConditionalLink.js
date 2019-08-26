af2dfe550d7c017edc3b19ce748448805c969c27
Jack Fletcher <jdfletch97@gmail.com>
2019-08-26 01:01:41 -0500

    Add className prop to ConditionalLink

--- components/ConditionalLink.js
+++ components/ConditionalLink.js
@@ -2,19 +2,24 @@ import React from 'react';
 import {forwardRef} from 'react';
 import Link from 'next/link';
 
-const Anchor = forwardRef(({children, href, style}, ref) => (
+const Anchor = forwardRef(({children, className, href, style}, ref) => (
   <a
     ref={ref}
     href={href}
     style={{...style, color: 'inherit', textDecoration: 'none'}}
+    className={className}
   >
     {children}
   </a>
 ));
 Anchor.displayName = 'Anchor';
 
-const ConditionalLink = ({as, children, href, style}) => {
-  const anchor = <Anchor style={style}>{children}</Anchor>;
+const ConditionalLink = ({as, children, className, href, style}) => {
+  const anchor = (
+    <Anchor className={className} style={style}>
+      {children}
+    </Anchor>
+  );
   const externalUrl = href.indexOf('http') === 0;
   if (externalUrl) {
     return React.cloneElement(anchor, {href});
