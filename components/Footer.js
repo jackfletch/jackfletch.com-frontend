ba98aa6a9682eafa08d0af29c59642a9ee788849
Jack Fletcher <jdfletch97@gmail.com>
2019-08-26 15:46:02 -0500

    Refactor Footer styles to css

--- components/Footer.js
+++ components/Footer.js
@@ -1,46 +1,23 @@
-import styled from 'styled-components';
 import Copyright from './Copyright';
 import FooterIconLink from './FooterIconLink';
 import websiteConfig from '../config/website';
-
-const Div = styled.div`
-  padding-top: 120px;
-  padding-bottom: 120px;
-
-  @media screen and (max-width: 991px) {
-    padding-top: 60px;
-    padding-bottom: 60px;
-  }
-`;
-
-const SFooter = styled.footer`
-  background-color: #665500;
-  color: #eee;
-  text-align: center;
-`;
-
-const Ul = styled.ul`
-  padding: 0;
-  margin: 0;
-  text-align: center;
-`;
 
 const Footer = () => {
   const networks = Object.keys(websiteConfig.author.contacts);
   const owner = websiteConfig.author.name;
 
   return (
-    <SFooter className="bottom">
-      <Div className="container">
-        <p>STAY CONNECTED</p>
-        <Ul>
+    <footer>
+      <div className="footer__links">
+        <p>Stay Connected</p>
+        <ul>
           {networks.map(network => (
             <FooterIconLink key={network} network={network} />
           ))}
-        </Ul>
-      </Div>
+        </ul>
+      </div>
       <Copyright date={new Date().getFullYear()} owner={owner} />
-    </SFooter>
+    </footer>
   );
 };
 
