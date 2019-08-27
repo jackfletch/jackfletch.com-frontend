465c73ecb080408446bd0fde74d0fb809f42812c
Jack Fletcher <jdfletch97@gmail.com>
2019-08-27 16:39:14 -0500

    Fix SSR rendering of html and head
    Major bug fix. See https://github.com/zeit/next.js#custom-document.

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, {Main, NextScript} from 'next/document';
+import Document, {Html, Head, Main, NextScript} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -29,12 +29,13 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
