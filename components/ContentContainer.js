f2ae98445806910a151e80aaa646c2ff135fd0a3
Jack Fletcher <jdfletch97@gmail.com>
2019-09-10 23:13:23 -0500

    Add prop-types to ContentContainer component

--- components/ContentContainer.js
+++ components/ContentContainer.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const OuterDiv = styled.div`
@@ -23,6 +24,13 @@ const ContentContainer = ({className, innerStyle, outerStyle, children}) => {
       </InnerDiv>
     </OuterDiv>
   );
+};
+
+ContentContainer.propTypes = {
+  classNames: PropTypes.string,
+  innerStyle: PropTypes.object,
+  outerStyle: PropTypes.object,
+  children: PropTypes.node,
 };
 
 export default ContentContainer;
