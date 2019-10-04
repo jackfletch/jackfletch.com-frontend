fba28131720bf6289ba3a10a7c4e51d6ca09c25d
Jack Fletcher <jdfletch97@gmail.com>
2019-10-04 18:10:51 -0500

    Add script to generate sitemap (#21)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const withCSS = require('@zeit/next-css');
 const bundleAnalyzer = require('@next/bundle-analyzer');
@@ -7,12 +8,28 @@ const rehypeCodeSnippetIds = require('./lib/rehypeCodeSnippetIds');
 const rehypeHighlight = require('rehype-highlight');
 const rehypeSlug = require('rehype-slug');
 
+const {createPagesMapping} = require('next/dist/build/entries');
+const {collectPages} = require('next/dist/build/utils');
+
+const PAGES_DIR = path.join(process.cwd(), 'pages');
+
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
+async function getPages() {
+  const pagePaths = await collectPages(PAGES_DIR, nextConfig.pageExtensions);
+  const mappedPages = createPagesMapping(pagePaths, nextConfig.pageExtensions);
+  const pageKeys = Object.keys(mappedPages);
+  const routablePages = pageKeys.filter(
+    page => !page.match(/^\/(_app|_error|_document|api)/)
+  );
+  return routablePages;
+}
+
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  getPages: getPages,
   webpack: (config, {defaultLoaders}) => {
     config.module.rules.push({
       test: /pages\/blog\/.*\.mdx?$/,
