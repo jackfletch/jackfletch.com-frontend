7a67e3234b6c7e25084496ef671e4d25a90ce1ec
Jack Fletcher <jdfletch97@gmail.com>
2019-10-06 01:59:57 -0500

    Change blog post previews to be statically loaded

--- pages/blog.js
+++ pages/blog.js
@@ -1,76 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import fetch from 'isomorphic-unfetch';
-import styled from 'styled-components';
 import {BlogCard, ContentContainer, Meta, Page} from '../components';
 import config from '../config/website';
-import {getBaseUrl} from '../lib';
 
-const P = styled.p`
-  margin-top: 0.5rem;
-  margin-bottom: 0;
-  font-size: 0.875rem;
-`;
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+function dateSortDesc(a, b) {
+  const dateA = new Date(a.date);
+  const dateB = new Date(b.date);
+  return dateA < dateB ? 1 : -1;
+}
+
+const posts = importAll(require.context('./blog', false, /\.mdx?$/))
+  .map(post => post.frontMatter)
+  .sort(dateSortDesc)
+  .map(post => <BlogCard key={post.slug} post={post} />);
 
 const innerContentContainerStyle = {
   maxWidth: '720px',
 };
 
-class BlogPage extends React.Component {
-  static propTypes = {
-    posts: PropTypes.arrayOf(
-      PropTypes.shape({
-        date: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        slug: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-      })
-    ).isRequired,
+const BlogPage = () => {
+  const title = `Blog | ${config.title}`;
+  const schema = {
+    '@context': 'https://schema.org',
+    '@type': 'Blog',
+    name: 'Ramblings from Jack',
+    description:
+      "Random posts about things I'm involved with or trying to understand",
+    url: `${config.url}/blog`,
+    author: {
+      '@context': 'https://schema.org',
+      '@type': 'Person',
+      name: config.author.name,
+      url: config.url,
+      sameAs: [
+        `https://twitter.com/${config.author.contacts.twitter}`,
+        `https://instagram.com/${config.author.contacts.instagram}`,
+        `https://linkedin.com/in/${config.author.contacts.linkedin}`,
+      ],
+    },
   };
 
-  static async getInitialProps({req}) {
-    const baseUrl = getBaseUrl(req);
-    const posts = await fetch(`${baseUrl}/api/posts`).then(r => r.json());
-
-    return {posts};
-  }
-
-  render() {
-    const {posts} = this.props;
-    const title = `Blog | ${config.title}`;
-    const schema = {
-      '@context': 'https://schema.org',
-      '@type': 'Blog',
-      name: 'Ramblings from Jack',
-      description:
-        "Random posts about things I'm involved with or trying to understand",
-      url: `${config.url}/blog`,
-      author: {
-        '@context': 'https://schema.org',
-        '@type': 'Person',
-        name: config.author.name,
-        url: config.url,
-        sameAs: [
-          `https://twitter.com/${config.author.contacts.twitter}`,
-          `https://instagram.com/${config.author.contacts.instagram}`,
-          `https://linkedin.com/in/${config.author.contacts.linkedin}`,
-        ],
-      },
-    };
-
-    return (
-      <>
-        <Meta schema={schema} title={title} />
-        <Page>
-          <ContentContainer innerStyle={innerContentContainerStyle}>
-            {posts.map(post => (
-              <BlogCard key={post.slug} post={post} />
-            ))}
-          </ContentContainer>
-        </Page>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Meta schema={schema} title={title} />
+      <Page>
+        <ContentContainer innerStyle={innerContentContainerStyle}>
+          {posts}
+        </ContentContainer>
+      </Page>
+    </>
+  );
+};
 
 export default BlogPage;
