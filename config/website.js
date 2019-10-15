e47a332bd3f7b4708e9be539051bf0d4599499fd
Jack Fletcher <jdfletch97@gmail.com>
2019-10-15 02:19:24 -0500

    Fix storage bucket URL endpoint

--- config/website.js
+++ config/website.js
@@ -14,7 +14,7 @@ const websiteConfig = {
   title: 'Jack Fletcher',
   description:
     "CPE '19 @ Mississippi State. I blog about web development, machine learning, visualization, and more.",
-  storageBucketUrl: 'https://storage.cloud.google.com/jackfletch',
+  storageBucketUrl: 'https://storage.googleapis.com/jackfletch',
   menu: [
     {
       label: 'Welcome',
