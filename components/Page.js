4a4b790c99afaa50d7caf8d4435f7577847f7c8c
Jack Fletcher <jdfletch97@gmail.com>
2019-10-16 11:24:19 -0500

    Remove PageLayout component

--- components/Page.js
+++ components/Page.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+
 import Header from './Header';
 import Footer from './Footer';
 
-const PageLayout = styled.div`
-  scroll-behavior: smooth;
-`;
-
 const Page = ({children, noHeader}) => (
-  <PageLayout className="page">
+  <>
     {noHeader ? null : <Header />}
     {children}
     <Footer />
-  </PageLayout>
+  </>
 );
 
 Page.propTypes = {
