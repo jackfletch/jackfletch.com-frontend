3d5057eaee03551b94dd3244a5a0d3caec903451
Jack Fletcher <jdfletch97@gmail.com>
2019-10-16 15:25:12 -0500

    Refactor footer components

--- components/Copyright.js
+++ components/Copyright.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  background-color: ${props => props.theme.colors.blacks[1]};
+  padding: 1rem;
+`;
 
 const Copyright = ({date, owner}) => (
-  <small id="copyright">
-    &copy; {date} {owner}
-  </small>
+  <Div>
+    <small id="copyright">
+      &copy; {date} {owner}
+    </small>
+  </Div>
 );
 
 export default Copyright;
