d6c58f8556a8263f0988f62c43d71956297b83f3
Jack Fletcher <jdfletch97@gmail.com>
2019-10-17 13:59:11 -0500

    Replace bootstrap.css with styled-normalize
    Only a few parts of bootstrap were being used: reboot, card-link,
    nav-link, and container/container-fluid. These pieces have already been
    or will be readded to the project (most likely as styled-components) in
    the neighboring commits.
    
    This commit reimplements the style reset (as styled-normalize) and a few
    random bits of global style that depend on the styled-components theme.
    
    This is possible thanks to styled-components's `createGlobalStyle`
    function. However, this method has a couple disadvantages due to
    styled-components's inlined nature:
    
    - The style reset and static global styles override external css
      like highlight.js's stylesheet.
    
    - These styles, which are global throughout the site, are listed
      in full in every page's head. In contrast, if they were CSS files, they could be
      loaded once, cached, and loaded in a fraction of the time.
    
    In the future, it would be wise to refactor the style reset and the
    static global styles (default typography, spacing, etc.) into CSS files.
    Track zeit/next.js#8626 for updates on better support for external CSS
    in Next.js.

--- pages/_app.js
+++ pages/_app.js
@@ -3,22 +3,109 @@
 // for some reason this can't go in _document.js.
 import App from 'next/app';
 import Router from 'next/router';
-import {ThemeProvider} from 'styled-components';
+import {createGlobalStyle, ThemeProvider} from 'styled-components';
+import {normalize} from 'styled-normalize';
 
 import theme from '../data/theme';
 import {gtag} from '../lib';
-import '../static/css/bootstrap.css';
 import '../static/css/main.css';
 import '../static/css/screen.css';
 import '../static/css/highlight.css';
 
 Router.events.on('routeChangeComplete', url => gtag.trackPageview(url));
 
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+    font-family: inherit;
+  }
+
+  body {
+    background-color: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text.prose};
+    font-size: 1rem;
+    line-height: 1.5;
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: rgba(3, 102, 214, 1);
+  }
+
+  a:hover {
+    text-decoration: underline rgba(3, 102, 214, 0.6);
+  }
+
+  a[href^='#']:hover {
+    text-decoration-style: dashed;
+  }
+
+  pre,
+  code,
+  kbd,
+  samp,
+  tt {
+    font-family: SFMono-Regular, Menlo, Monaco, monospace;
+  }
+
+  pre {
+    line-height: 1.4;
+    overflow-x: auto;
+    color: ${props => props.theme.colors.blacks[1]};
+    padding: 1rem;
+
+    @media screen and (max-width: ${props => props.theme.breakpoints[0]}) {
+      margin: 0 -1rem;
+      border-radius: 0;
+    }
+  }
+
+  code {
+    padding: 3px 6px;
+    font-size: 0.875em;
+  }
+
+  pre,
+  code {
+    background-color: rgba(0, 0, 0, 0.04);
+    border-radius: 3px;
+  }
+
+  pre > code {
+    color: inherit;
+    word-break: normal;
+    border-radius: 0;
+    background-color: transparent;
+    padding: 0;
+  }
+
+  div.post ul > li {
+    margin-left: 1.4em;
+  }
+
+  div.post li {
+    margin-left: 2em;
+    margin-bottom: 0.2em;
+  }
+`;
+
 class CustomApp extends App {
   render() {
     const {Component, pageProps} = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     );
