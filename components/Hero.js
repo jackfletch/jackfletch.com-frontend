ac7aca43b277cb271d0be46c0f197d1b9573f2c6
Jack Fletcher <jdfletch97@gmail.com>
2020-02-12 20:19:23 -0600

    Change layout of hero element

--- components/Hero.js
+++ components/Hero.js
@@ -31,6 +31,35 @@ const P = styled.p`
   line-height: 1.75;
 `;
 
+const ListDiv = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  padding: 16px;
+  max-width: 800px;
+  text-align: center;
+`;
+
+const Ul = styled.ul`
+  list-style-type: none;
+  padding-left: 0px;
+`;
+
+const Li = styled.li`
+  font-size: 1.25rem;
+  margin-right: 16px;
+  display: inline-block;
+
+  &::before {
+    content: '•';
+    color: rgb(222, 226, 230);
+    margin-right: 16px;
+  }
+  :first-child::before {
+    content: '';
+    margin-right: 0px;
+  }
+`;
+
 const Hero = () => (
   <>
     <HeroDiv>
@@ -38,26 +67,31 @@ const Hero = () => (
         <H1>Jack Fletcher</H1>
         <P>I'm a Software Engineer from Memphis, TN.</P>
         <P>I make usable tools and visualizations with code.</P>
-        <P>
+      </MessageDiv>
+    </HeroDiv>
+    <ListDiv>
+      <Ul>
+        <Li>
           <Link href="/blog">
-            <a>blog</a>
+            <a>Blog</a>
           </Link>
-          ,{' '}
+        </Li>
+        <Li>
           <Link href="/projects">
-            <a>projects</a>
+            <a>Projects</a>
           </Link>
-          , and{' '}
+        </Li>
+        <Li>
           <a
             href={`${storageBucketUrl}/static/documents/JacksonFletcherResume.pdf`}
             target="_blank"
             rel="noopener noreferrer"
           >
-            résumé
+            Résumé
           </a>
-          .
-        </P>
-      </MessageDiv>
-    </HeroDiv>
+        </Li>
+      </Ul>
+    </ListDiv>
   </>
 );
 
