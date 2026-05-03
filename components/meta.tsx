daf78f8ed213892560be64ed241006561e880e09
Jack Fletcher <jdfletch97@gmail.com>
2026-05-03 16:44:01 -0700

    Move gtag scripts from next/head to next/script
    next/head does not support <script> tags. Use next/script with the
    afterInteractive strategy for Google Analytics instead.

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,17 +1,9 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import {useRouter} from 'next/router';
 import config from '../config/website';
 import {GA_TRACKING_ID} from '../config/env';
 import {stripUrlParams} from '../lib';
-
-function setGoogleTags() {
-  return {
-    __html: `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${GA_TRACKING_ID}');`,
-  };
-}
 
 interface MetaProps {
   description?: string;
@@ -31,55 +23,59 @@ const Meta = ({
   const router = useRouter();
 
   return (
-  <Head>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>{title}</title>
-    <meta name="description" content={description} />
-    <meta name="theme-color" content="#665500" />
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="theme-color" content="#665500" />
 
-    {/* open graph */}
-    <meta property="og:title" content={title} />
-    <meta property="og:site_name" content={config.title} />
-    <meta property="og:type" content={type} />
-    <meta
-      property="og:url"
-      content={`${config.url}${stripUrlParams(router.asPath)}`}
-    />
-    <meta property="og:description" content={description} />
-    {image && <meta property="og:image" content={`${config.url}${image}`} />}
+        {/* open graph */}
+        <meta property="og:title" content={title} />
+        <meta property="og:site_name" content={config.title} />
+        <meta property="og:type" content={type} />
+        <meta
+          property="og:url"
+          content={`${config.url}${stripUrlParams(router.asPath)}`}
+        />
+        <meta property="og:description" content={description} />
+        {image && <meta property="og:image" content={`${config.url}${image}`} />}
 
-    {/* twitter */}
-    <meta name="twitter:site" content={`@${config.author.contacts.twitter}`} />
-    <meta
-      name="twitter:creator"
-      content={`@${config.author.contacts.twitter}`}
-    />
-    <meta name="twitter:card" content="summary_large_image" />
+        {/* twitter */}
+        <meta name="twitter:site" content={`@${config.author.contacts.twitter}`} />
+        <meta
+          name="twitter:creator"
+          content={`@${config.author.contacts.twitter}`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
 
-    {/* favicons */}
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="256x256" href="/img/icon-256x256.png" />
+        {/* favicons */}
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="256x256" href="/img/icon-256x256.png" />
 
-    {/* schema.org schema */}
-    {schema && (
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{__html: JSON.stringify(schema)}}
+        {/* schema.org schema */}
+        {schema && (
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{__html: JSON.stringify(schema)}}
+          />
+        )}
+
+        {/* RSS feed */}
+        <link rel="alternate" type="application/rss+xml" href="/rss.xml" />
+      </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
       />
-    )}
-
-    {/* RSS feed */}
-    <link rel="alternate" type="application/rss+xml" href="/rss.xml" />
-
-    {/* scripts */}
-    {/* Global Site Tag (gtag.js) - Google Analytics */}
-    <script
-      async
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-    />
-    <script dangerouslySetInnerHTML={setGoogleTags()} />
-  </Head>
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}');`}
+      </Script>
+    </>
   );
 };
 
